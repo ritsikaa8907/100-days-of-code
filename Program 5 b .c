165fa2cd87fe9e1ac0673a67169b61d222139c0b
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-02-11 19:15:47 +0530

    ritsika

---
+++ Program 5 b .c
@@ -0,0 +1,25 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+void merge(int* nums1, int m, int* nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+

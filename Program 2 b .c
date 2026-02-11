61fadc0ef9b6a7e09de3ba9b9b12bdbea7d17d6d
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-02-11 19:13:28 +0530

    ritsika

---
+++ Program 2 b .c
@@ -0,0 +1,15 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice)
+            minPrice = prices[i];
+        else if (prices[i] - minPrice > maxProfit)
+            maxProfit = prices[i] - minPrice;
+    }
+
+    return maxProfit;
+}

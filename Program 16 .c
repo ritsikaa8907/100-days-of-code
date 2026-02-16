282594c2fa367790a7b4543a4fe8d98a9bbcae44
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-02-16 21:12:00 +0530

    ritsika

---
+++ Program 16 .c
@@ -0,0 +1,37 @@
+//Given an array of integers, count the frequency of each distinct element and print the result.
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int threeSumClosest(int* nums, int numsSize, int target) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int closestSum = nums[0] + nums[1] + nums[2];
+
+    for (int i = 0; i < numsSize - 2; i++) {
+        int left = i + 1;
+        int right = numsSize - 1;
+
+        while (left < right) {
+            int currentSum = nums[i] + nums[left] + nums[right];
+
+            if (abs(currentSum - target) < abs(closestSum - target)) {
+                closestSum = currentSum;
+            }
+
+            if (currentSum < target) {
+                left++;
+            } else if (currentSum > target) {
+                right--;
+            } else {
+                return currentSum; 
+            }
+        }
+    }
+
+    return closestSum;
+}

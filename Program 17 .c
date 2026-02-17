c318309a55baec6aecf9b6b995ecff67fb6ba833
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-02-17 18:12:49 +0530

    ritsika

---
+++ Program 17 .c
@@ -0,0 +1,38 @@
+//Write a program to find the maximum and minimum values present in a given array of integers.
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid array size\n");
+        return 0;
+    }
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Maximum value: %d\n", max);
+    printf("Minimum value: %d\n", min);
+
+    return 0;
+}

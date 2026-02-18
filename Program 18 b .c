d5ebfb6a4751bb3b1309e84e0e207058615af2d7
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-02-18 19:07:06 +0530

    ritsika

---
+++ Program 18 b .c
@@ -0,0 +1,21 @@
+//Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+#include <stdlib.h>
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+
+    int* answer = (int*)malloc(numsSize * sizeof(int));
+
+    answer[0] = 1;
+    for (int i = 1; i < numsSize; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    int suffix = 1;
+    for (int i = numsSize - 1; i >= 0; i--) {
+        answer[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return answer;
+}

6a002c44ee126ab76c265c8db28097bfd8e4c6b2
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-02-21 22:40:45 +0530

    ritsika

---
+++ Program 21 .c
@@ -0,0 +1,55 @@
+//Create and Traverse Singly Linked List
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
+
+    if (head == NULL) {
+        return newNode;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    return head;
+}
+
+void traverse(struct Node* head) {
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    head = insertEnd(head, 10);
+    head = insertEnd(head, 20);
+    head = insertEnd(head, 30);
+    head = insertEnd(head, 40);
+
+    printf("Linked List: ");
+    traverse(head);
+
+    return 0;
+}

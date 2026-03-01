f676436559dab23b2b36eee1b7780ed6b37905e1
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-03-01 16:53:41 +0530

    ritsika

---
+++ Program 28 b .c
@@ -0,0 +1,33 @@
+//Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+bool isPalindrome(struct ListNode* head) {
+    if (head == NULL || head->next == NULL)
+        return true;
+
+    struct ListNode *slow = head, *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    struct ListNode *prev = NULL, *curr = slow, *next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    struct ListNode *first = head;
+    struct ListNode *second = prev;
+
+    while (second != NULL) {
+        if (first->val != second->val)
+            return false;
+        first = first->next;
+        second = second->next;
+    }
+
+    return true;
+}

41828386e05b8a4c5d5a03f8a45115149679b7bd
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-03-07 11:28:07 +0530

    ritsika

---
+++ Program 35 .c
@@ -0,0 +1,69 @@
+//Queue Using Array - Implement using linked list with dynamic memory allocation.
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node *front = NULL, *rear = NULL;
+
+void enqueue(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (rear == NULL) {
+        front = rear = newNode;
+        return;
+    }
+
+    rear->next = newNode;
+    rear = newNode;
+}
+
+void dequeue() {
+    if (front == NULL) {
+        printf("Queue Underflow\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("Deleted: %d\n", front->data);
+
+    front = front->next;
+
+    if (front == NULL)
+        rear = NULL;
+
+    free(temp);
+}
+
+void display() {
+    struct Node* temp = front;
+
+    if (temp == NULL) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+    display();
+
+    return 0;
+}

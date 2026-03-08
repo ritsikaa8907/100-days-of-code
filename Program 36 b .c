f4fcb1477b490789142ea2d0a401ce07fc3c7501
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-03-08 12:28:10 +0530

    ritsika

---
+++ Program 36 b .c
@@ -0,0 +1,99 @@
+//Design your implementation of the circular double-ended queue (deque).
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+} MyCircularDeque;
+
+MyCircularDeque* myCircularDequeCreate(int k) {
+    MyCircularDeque* obj = (MyCircularDeque*)malloc(sizeof(MyCircularDeque));
+    obj->arr = (int*)malloc(sizeof(int) * k);
+    obj->front = -1;
+    obj->rear = -1;
+    obj->size = 0;
+    obj->capacity = k;
+    return obj;
+}
+
+bool myCircularDequeInsertFront(MyCircularDeque* obj, int value) {
+    if (obj->size == obj->capacity) return false;
+
+    if (obj->size == 0) {
+        obj->front = obj->rear = 0;
+    } else {
+        obj->front = (obj->front - 1 + obj->capacity) % obj->capacity;
+    }
+
+    obj->arr[obj->front] = value;
+    obj->size++;
+    return true;
+}
+
+bool myCircularDequeInsertLast(MyCircularDeque* obj, int value) {
+    if (obj->size == obj->capacity) return false;
+
+    if (obj->size == 0) {
+        obj->front = obj->rear = 0;
+    } else {
+        obj->rear = (obj->rear + 1) % obj->capacity;
+    }
+
+    obj->arr[obj->rear] = value;
+    obj->size++;
+    return true;
+}
+
+bool myCircularDequeDeleteFront(MyCircularDeque* obj) {
+    if (obj->size == 0) return false;
+
+    if (obj->size == 1) {
+        obj->front = obj->rear = -1;
+    } else {
+        obj->front = (obj->front + 1) % obj->capacity;
+    }
+
+    obj->size--;
+    return true;
+}
+
+bool myCircularDequeDeleteLast(MyCircularDeque* obj) {
+    if (obj->size == 0) return false;
+
+    if (obj->size == 1) {
+        obj->front = obj->rear = -1;
+    } else {
+        obj->rear = (obj->rear - 1 + obj->capacity) % obj->capacity;
+    }
+
+    obj->size--;
+    return true;
+}
+
+int myCircularDequeGetFront(MyCircularDeque* obj) {
+    if (obj->size == 0) return -1;
+    return obj->arr[obj->front];
+}
+
+int myCircularDequeGetRear(MyCircularDeque* obj) {
+    if (obj->size == 0) return -1;
+    return obj->arr[obj->rear];
+}
+
+bool myCircularDequeIsEmpty(MyCircularDeque* obj) {
+    return obj->size == 0;
+}
+
+bool myCircularDequeIsFull(MyCircularDeque* obj) {
+    return obj->size == obj->capacity;
+}
+
+void myCircularDequeFree(MyCircularDeque* obj) {
+    free(obj->arr);
+    free(obj);
+}

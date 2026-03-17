cd13e66376d3138204db8ee7330dc71d858778bc
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-03-17 14:28:38 +0530

    ritsika

---
+++ program 45 b .c
@@ -0,0 +1,19 @@
+//Given the root of a binary tree, return its maximum depth.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return 1 + (left > right ? left : right);
+}

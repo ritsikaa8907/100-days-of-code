81eac1d49a2ef869f5b3a09d6abcc7b3cca9b3c6
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-03-20 08:17:53 +0530

    ritsika

---
+++ Program 48 b .c
@@ -0,0 +1,22 @@
+//Given the root of a binary tree, return the sum of all left leaves.
+
+int sumOfLeftLeaves(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int sum = 0;
+
+    if (root->left != NULL) {
+        if (root->left->left == NULL && root->left->right == NULL) {
+            sum += root->left->val;
+        } else {
+            sum += sumOfLeftLeaves(root->left);
+        }
+    }
+
+    sum += sumOfLeftLeaves(root->right);
+
+    return sum;
+}
+ 

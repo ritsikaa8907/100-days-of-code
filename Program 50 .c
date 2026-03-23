a7e203840b9ab0ff75b83c3ceec4155a52931d40
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-03-23 18:28:11 +0530

    ritsika

---
+++ Program 50 .c
@@ -0,0 +1,69 @@
+//Problem: BST Search
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+int search(struct Node* root, int key) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    if (root->data == key) {
+        return 1;
+    }
+
+    if (key < root->data) {
+        return search(root->left, key);
+    } else {
+        return search(root->right, key);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    int key = 40;
+
+    if (search(root, key)) {
+        printf("Element found\n");
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}

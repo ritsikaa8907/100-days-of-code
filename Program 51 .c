ca47e9c1a9c1efc11beaa96cea2bee14dd7e2e66
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-03-23 18:30:32 +0530

    ritsika

---
+++ Program 51 .c
@@ -0,0 +1,13 @@
+//Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Search Tree.
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    while (root != NULL) {
+        if (p->val < root->val && q->val < root->val) {
+            root = root->left;
+        } else if (p->val > root->val && q->val > root->val) {
+            root = root->right;
+        } else {
+            return root;
+        }
+    }
+    return NULL;
+}

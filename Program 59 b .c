06a101b314c045aeb8dc814bda7027c7199e94f6
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-04-01 08:18:41 +0530

    ritsika

---
+++ Program 59 b .c
@@ -0,0 +1,46 @@
+//Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+#include <stdlib.h>
+
+int findIndex(int* inorder, int start, int end, int val) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == val)
+            return i;
+    }
+    return -1;
+}
+
+struct TreeNode* build(int* inorder, int* postorder,
+                       int inStart, int inEnd,
+                       int* postIndex) {
+
+    if (inStart > inEnd)
+        return NULL;
+
+    int rootVal = postorder[*postIndex];
+    (*postIndex)--;
+
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = rootVal;
+    root->left = root->right = NULL;
+
+    int inIndex = findIndex(inorder, inStart, inEnd, rootVal);
+
+    root->right = build(inorder, postorder,
+                        inIndex + 1, inEnd, postIndex);
+
+    root->left = build(inorder, postorder,
+                       inStart, inIndex - 1, postIndex);
+
+    return root;
+}
+
+struct TreeNode* buildTree(int* inorder, int inorderSize,
+                           int* postorder, int postorderSize) {
+
+    int postIndex = postorderSize - 1;
+
+    return build(inorder, postorder,
+                 0, inorderSize - 1,
+                 &postIndex);
+}

37cc45da8367c489fe2267f79918471dc5984420
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-04-04 11:13:59 +0530

    Create Program 63 .c

---
+++ Program 63 .c
@@ -0,0 +1,71 @@
+//Problem: Perform DFS starting from a given source vertex using recursion.
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adj[MAX];
+int visited[MAX];
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+void dfs(int vertex) {
+    visited[vertex] = 1;
+    printf("%d ", vertex);
+
+    struct Node* temp = adj[vertex];
+    while(temp) {
+        int adjVertex = temp->vertex;
+        if(!visited[adjVertex]) {
+            dfs(adjVertex);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m;
+
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &n, &m);
+
+    for(int i = 0; i < n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    printf("Enter edges (u v):\n");
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    int start;
+    printf("Enter starting vertex: ");
+    scanf("%d", &start);
+
+    printf("DFS Traversal: ");
+    dfs(start);
+
+    return 0;
+}

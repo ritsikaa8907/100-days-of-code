07f5c1fb581f6541c75d1bf7502929d2b7f3d888
Ritsika Raghuvanshi <ritsikaraghuvanshi7@gmail.com>
2026-04-23 00:12:47 +0530

    ritsika

---
+++ Program 80 .c
@@ -0,0 +1,49 @@
+//Problem: Given a weighted graph with n vertices, implement the Floyd-Warshall algorithm to compute the shortest distances between every pair of vertices. The graph may contain positive or negative edge weights, but it does not contain any negative weight cycles.
+#include <stdio.h>
+
+#define V 4
+#define INF 1000000000
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                    
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0,   3,   INF, 7},
+        {8,   0,   2,   INF},
+        {5,   INF, 0,   1},
+        {2,   INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
